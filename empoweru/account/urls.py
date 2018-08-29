7cb7f0a9dc5446245491ca23955ba417a157d52b
isultania <isultania@unomaha.edu>
2018-08-29 12:16:03 -0500

    Basic Dashboard

--- empoweru/account/urls.py
+++ empoweru/account/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    # post views
-    path('login/', views.user_login,name='login'),
+    # previous login view
+    #path('login/', views.user_login,name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', views.dashboard, name='dashboard'),
     ]
--- empoweru/empoweru/settings.py
+++ empoweru/empoweru/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
