b66878bab3ea0dda073a5245c7dc33a0be9cc75a
vnikesh <nikeev1991@gmail.com>
2018-09-18 21:17:46 -0500

    Changed admin by adding program model

--- empoweru/account/admin.py
+++ empoweru/account/admin.py
@@ -1,3 +1,17 @@
-from django.contrib import admin
+# from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Program
+
+# Register your models here.
+
+class ProgramList(admin.ModelAdmin):
+    list_display = ('program_name', 'program_start_date', 'program_end_date')
+    list_filter = ('program_name', 'program_start_date')
+    search_fields = ('program_name', 'program_start_date')
+    ordering = ['program_name']
+
+
+admin.site.register(Program, ProgramList)
+
