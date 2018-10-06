42344bcffc29e296fe0de3534b20aea42e4a2870
lelandhoran <lhoran@unomaha.edu>
2018-10-06 12:06:10 -0500

    Added wagtailmenus and updated UI

--- empoweru/settings.py
+++ empoweru/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'home',
     'week',
     'widget_tweaks',
+    'wagtailmenus',
 ]
 
 MIDDLEWARE = [
@@ -86,6 +87,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'wagtailmenus.context_processors.wagtailmenus',
             ],
         },
     },
