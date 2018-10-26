36485714ab70e8ef604be44063cb91397267c04b
adoll04 <adollweier@unomaha.edu>
2018-10-25 19:27:34 -0500

    had to change the database name

--- empoweru/settings.py
+++ empoweru/settings.py
@@ -126,7 +126,7 @@ WSGI_APPLICATION = 'empoweru.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'empoweru1',
+        'NAME': 'empoweru3',
         'USER': 'postgres',
         'PASSWORD': 'instructor1a',
         'HOST': 'localhost',
