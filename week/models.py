6b96c3f9b9decd8cc4217475c5a66a2eeae3d085
isultania <isultania@unomaha.edu>
2018-11-13 11:58:45 -0600

    Dont show image if its not there

--- week/models.py
+++ week/models.py
@@ -40,6 +40,7 @@ class ModelIndexPage(Page):
     ad_image = models.ForeignKey('wagtailimages.Image', null=True, blank=True, on_delete=models.SET_NULL,
                                       related_name='+')
 
+
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname="full"),
         ImageChooserPanel('display_image'),
