2751bac696327ab808832f98db1263a09951431b
harsh1930 <hchouhan@unomaha.edu>
2018-12-02 17:17:11 -0600

    setting

--- empoweru/settings.py
+++ empoweru/settings.py
@@ -175,7 +175,7 @@ USE_TZ = True
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'mysite/static'),
+    os.path.join(BASE_DIR, 'empoweru/static'),
 ]
 
 AWS_ACCESS_KEY_ID = 'AKIAILXVOKCUNQYIVZYA'
@@ -191,7 +191,7 @@ AWS_LOCATION = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
-DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'
+DEFAULT_FILE_STORAGE = 'empoweru.storage_backends.MediaStorage'
 # AWS_ACCESS_KEY_ID = 'AKIAILXVOKCUNQYIVZYA'
 # AWS_SECRET_ACCESS_KEY = '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'
 # AWS_STORAGE_BUCKET_NAME = 'empoweru-static'
