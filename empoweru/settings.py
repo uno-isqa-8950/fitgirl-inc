5e79bac222675959e3d54cd6f0f252d31a27c4c2
harsh1930 <hchouhan@unomaha.edu>
2018-12-02 17:56:03 -0600

    setting

--- empoweru/settings.py
+++ empoweru/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'xz@m8r3&j2kh@t+9^rxmrbvg+-c4dv5$_&*ru2d1n1jf$3(l_-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
+
+ALLOWED_HOSTS = ['.localhost','.herokuapp.com']
 
 # Application definition
 
