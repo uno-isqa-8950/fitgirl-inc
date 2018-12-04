b6bccdfb57de10b9886d9ae7ea37592d237ce1d7
harsh1930 <hchouhan@unomaha.edu>
2018-12-04 04:02:13 -0600

    Update admin.py

--- account/admin.py
+++ account/admin.py
@@ -3,7 +3,7 @@
 # Register your models here.
 from django.contrib import admin
 from .models import Program
-from .models import Profile, RegisterUser, Affirmations, InspirationalQuotes
+from .models import Profile, RegisterUser, Affirmation, InspirationalQuote
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
@@ -43,11 +43,11 @@ admin.site.register(RegisterUser)
 
 class AffirmationAdmin(admin.ModelAdmin):
     Quotes = 'Quotes'
-admin.site.register(Affirmations, AffirmationAdmin)
+admin.site.register(Affirmation, AffirmationAdmin)
 
 
 class InspirationalAdmin(admin.ModelAdmin):
     InspirationalQuote = 'Inspirational Quotes'
-admin.site.register(InspirationalQuotes, InspirationalAdmin)
+admin.site.register(InspirationalQuote, InspirationalAdmin)
 
 
