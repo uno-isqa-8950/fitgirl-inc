7ff7bd65937edd491cca317e43db95261e5bfca6
harsh1930 <hchouhan@unomaha.edu>
2018-12-04 02:21:47 -0600

    aws wagtail fix

--- empoweru/settings.py
+++ empoweru/settings.py
@@ -189,7 +189,12 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 
+AWS_HEADERS= {
+    'Access-Control-Allow-Origin': '*'
+}
+
 AWS_LOCATION = 'static'
+
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
