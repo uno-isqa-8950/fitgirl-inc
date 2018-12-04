b23b72ee8f375148f267909464754f913a2a7a9d
harsh1930 <hchouhan@unomaha.edu>
2018-12-04 15:18:31 -0600

    Update settings.py

--- empoweru/settings.py
+++ empoweru/settings.py
@@ -189,12 +189,16 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 
+AWS_HEADERS = {
+    'Access-Control-Allow-Origin': '*'
+}
+
 
 AWS_LOCATION = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
-DEFAULT_FILE_STORAGE = 'empoweru.storage_backends.MediaStorage'
+DEFAULT_FILE_STORAGE = 'fitgirl-inc.storage_backends.MediaStorage'
 
 
 
