5d3e130fa7b221fa09553722878c5855a367ed03
harsh1930 <hchouhan@unomaha.edu>
2018-12-08 02:27:43 -0600

    Update settings.py

--- empoweru/settings.py
+++ empoweru/settings.py
@@ -201,7 +201,7 @@ AWS_LOCATION = 'static'
 AWS_LOCATION = 'media'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-MEDIA_URL = "https://%s/" % (AWS_S3_CUSTOM_DOMAIN,AWS_LOCATION)
+MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 
 DEFAULT_FILE_STORAGE = 'storage_backends.MediaStorage'
 
