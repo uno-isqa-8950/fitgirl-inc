63ccd274e34b27a044d23d8552e05bf9cb41964e
harsh1930 <hchouhan@unomaha.edu>
2018-12-08 00:53:07 -0600

    Update storage_backends.py

--- empoweru/storage_backends.py
+++ empoweru/storage_backends.py
@@ -1,5 +1,9 @@
+from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
+Class StaticStorage(S3Boto3Storage):
+    location = settings.STATICFILES_LOCATION
+
 class MediaStorage(S3Boto3Storage):
-    location = 'media'
+    location = settings.MEDIAFILES_LOCATION
     file_overwrite = False
