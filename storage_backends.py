d9660dae2131e48591531a377f605eb0fee2c592
harsh1930 <hchouhan@unomaha.edu>
2018-12-08 02:43:20 -0600

    Update storage_backends.py

--- storage_backends.py
+++ storage_backends.py
@@ -1,5 +1,22 @@
+
+from django.conf import settings
+from django.contrib.staticfiles.storage import ManifestFilesMixin
 from storages.backends.s3boto3 import S3Boto3Storage
 
+
+class PatchedS3StaticStorage(S3Boto3Storage):
+    def _save(self, name, content):
+        if hasattr(content, 'seek') and hasattr(content, 'seekable') and content.seekable():
+            content.seek(0)
+        return super()._save(name, content)
+
+
+class CachedS3Storage(ManifestFilesMixin, PatchedS3StaticStorage):
+    pass
+
+
+class StaticStorage(S3Boto3Storage):
+    location = settings.STATICFILES_LOCATION
+
 class MediaStorage(S3Boto3Storage):
-    location = 'media'
-    file_overwrite = False
+    location = settings.MEDIAFILES_LOCATION
