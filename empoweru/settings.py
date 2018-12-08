00fab4784043b804b64a636161d4a14c155e93dd
Abrar Hussain <afrasat@unomaha.edu>
2018-12-08 02:15:05 -0600

    Update settings.py

--- empoweru/settings.py
+++ empoweru/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.postgres',
-
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
     'wagtail.contrib.modeladmin',
@@ -61,6 +60,7 @@ INSTALLED_APPS = [
     'wagtailmenus',
     'crispy_forms',
     'storages',
+    'compressor',
 ]
 
 MIDDLEWARE = [
