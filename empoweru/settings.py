00b7f9d3bab22a00e0c473bf5b2752c40be57671
Abrar Hussain <afrasat@unomaha.edu>
2018-12-08 11:29:07 -0600

    Update settings.py

--- empoweru/settings.py
+++ empoweru/settings.py
@@ -192,7 +192,7 @@ AWS_LOCATION = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
-DEFAULT_FILE_STORAGE = 'empoweru.storage_backends.MediaStorage'
+DEFAULT_FILE_STORAGE = 'storage_backends.MediaStorage'
 
 
 AWS_HEADERS = {
