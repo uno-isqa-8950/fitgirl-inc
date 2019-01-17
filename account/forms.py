4d75cc511cf2bfe0238915b05b2c9aaee0f7c646
Shamrose Makandar <31262996+shamrose@users.noreply.github.com>
2019-01-17 12:12:23 -0600

    Updated forms.py
    Updated forms.py in the profile edit form

--- account/forms.py
+++ account/forms.py
@@ -72,7 +72,7 @@ class ProfileEditForm(forms.ModelForm):
 
     class Meta:
         model = Profile
-        fields = ('bio', 'date_of_birth', 'address', 'zip', 'city', 'state', 'contact_no', 'age_group', 'school', 'photo')
+        fields = ('bio', 'date_of_birth', 'city', 'state', 'zip', 'day_phone', 'age_group', 'school', 'photo')
 
 
 class ProgramForm(forms.ModelForm):
