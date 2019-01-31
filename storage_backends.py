0a298223556a52038df0ad689ec2fb4e405d907f
Shamrose Makandar <31262996+shamrose@users.noreply.github.com>
2019-01-31 17:06:48 -0600

    Media Root Changes

--- storage_backends.py
+++ storage_backends.py
@@ -3,8 +3,5 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 StaticRootS3BotoStorage = lambda: S3Boto3Storage(location='static')
 
-class S3MediaStorage(S3Boto3Storage):
-    def __init__(self, **kwargs):
-        kwargs['location'] = kwargs.get('location', 
-            settings.MEDIA_ROOT.replace('/', ''))
-        super(S3MediaStorage, self).__init__(**kwargs)
+MediaRootS3BotoStorage = lambda: S3Boto3Storage(location='media')
+
