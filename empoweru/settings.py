cbe853ec6c9c9dcd465b4b081b91c9c36a0cfcf6
Shamrose Makandar <31262996+shamrose@users.noreply.github.com>
2019-01-31 16:45:43 -0600

    Updated Settings.py

--- empoweru/settings.py
+++ empoweru/settings.py
@@ -178,10 +178,11 @@ AWS_S3_OBJECT_PARAMETERS = {
 
 AWS_LOCATION = 'static'
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'empoweru.storage_backends.StaticRootS3BotoStorage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
-DEFAULT_FILE_STORAGE = 'storage_backends.MediaStorage'
+DEFAULT_FILE_STORAGE = 'empoweru.storage_backends.MediaRootS3BotoStorage'
+
 
 
 AWS_HEADERS = {
