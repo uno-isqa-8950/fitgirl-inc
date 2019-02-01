2b6ea726ddf4e61def825cd8f3504d20bc868014
Shamrose Makandar <31262996+shamrose@users.noreply.github.com>
2019-01-31 22:23:12 -0600

    Changed Media Storage

--- storage_backends.py
+++ storage_backends.py
@@ -7,7 +7,7 @@ class StaticStorage(S3Boto3Storage):
     default_acl = 'public-read'
 
 
-class PublicMediaStorage(S3Boto3Storage):
+class MediaStorage(S3Boto3Storage):
     location = 'media'
     default_acl = 'public-read'
     file_overwrite = False
