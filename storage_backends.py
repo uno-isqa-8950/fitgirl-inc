1cdf31798d1d20d2cdb4f2bee9def09177970cd8
Shamrose Makandar <31262996+shamrose@users.noreply.github.com>
2019-01-31 19:46:07 -0600

    Update storage backends

--- storage_backends.py
+++ storage_backends.py
@@ -1,7 +1,7 @@
 
 from storages.backends.s3boto3 import S3Boto3Storage
 
-StaticRootS3BotoStorage = lambda: S3Boto3Storage(location='static')
-
-MediaRootS3BotoStorage = lambda: S3Boto3Storage(location='media')
+class MediaStorage(S3Boto3Storage):
+    location = 'media'
+    file_overwrite = False
 
