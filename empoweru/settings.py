6add7bbd7e979b6d53a6fe5a4dab0b8b8816acbd
Shamrose Makandar <31262996+shamrose@users.noreply.github.com>
2019-01-31 19:40:36 -0600

    Revert settings.py
    Revert settings.py to previous settings

--- empoweru/settings.py
+++ empoweru/settings.py
@@ -161,11 +161,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-SECURE_SSL_REDIRECT = True
 
-AWS_DEFAULT_ACL = None
-
-AWS_STORAGE_BUCKET_NAME = 'fitgirl-inc'
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
@@ -178,11 +175,10 @@ AWS_S3_OBJECT_PARAMETERS = {
 
 AWS_LOCATION = 'static'
 
-STATICFILES_STORAGE = 'storage_backends.StaticRootS3BotoStorage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
-DEFAULT_FILE_STORAGE = 'storage_backends.S3Boto3Storage'
-
+DEFAULT_FILE_STORAGE = 'storage_backends.MediaStorage'
 
 
 AWS_HEADERS = {
@@ -191,9 +187,9 @@ AWS_HEADERS = {
 # STATIC_ROOT = os.path.join(BASE_DIR, 'stat
 
 
-MEDIA_LOCATION = 'media'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-MEDIA_URL = AWS_S3_CUSTOM_DOMAIN + '/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 LOGIN_REDIRECT_URL = 'dashboard'
@@ -221,5 +217,4 @@ EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
 try:
     from empoweru.local_settings import *
 except ImportError:
-    raise Exception("A local_settings.py file is required to run this project")
-'''
+    raise Exception("A local_settings.py file is required to run this project"
