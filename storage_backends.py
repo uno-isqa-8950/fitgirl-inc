3b3fe290db6ef19f296a2300474ee0dab1979d6f
Shamrose Makandar <31262996+shamrose@users.noreply.github.com>
2019-01-31 20:30:38 -0600

    Updated storage_backends

--- storage_backends.py
+++ storage_backends.py
@@ -1,7 +1,14 @@
-
 from storages.backends.s3boto3 import S3Boto3Storage
+from django.conf import settings
 
-class MediaStorage(S3Boto3Storage):
+
+class StaticStorage(S3Boto3Storage):
+    location = 'static'
+    default_acl = 'public-read'
+
+
+class PublicMediaStorage(S3Boto3Storage):
     location = 'media'
+    default_acl = 'public-read'
     file_overwrite = False
 
