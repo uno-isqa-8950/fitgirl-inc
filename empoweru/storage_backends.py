4b9e8e5580529d8444b2acf7bb97b1d596ed0031
Shamrose Makandar <31262996+shamrose@users.noreply.github.com>
2019-02-05 14:54:57 -0600

    Updated  storage_backends

--- empoweru/storage_backends.py
+++ empoweru/storage_backends.py
@@ -2,7 +2,12 @@ from storages.backends.s3boto3 import S3Boto3Storage
 from django.conf import settings
 
 
+class StaticStorage(S3Boto3Storage):
+    location = 'static'
+    default_acl = 'public-read'
 
-class MediaStorage(S3Boto3Storage):
+
+class PublicMediaStorage(S3Boto3Storage):
     location = 'media'
+    default_acl = 'public-read'
     file_overwrite = False
