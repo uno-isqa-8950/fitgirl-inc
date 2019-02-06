2384630326ecee23b6b5af54e33f28d873f493cc
karthik <kbellaryvijaya@gmail.com>
2019-02-05 19:17:37 -0600

    fix the max width for the footer

--- account/static/js/script.js
+++ account/static/js/script.js
@@ -1,29 +1,30 @@
-
-$( document ).ready(function() {
+$(document).ready(function() {
 
     // Affirmations popup code starts here - Karthik
-                    if($("#userLandingContent1").length == 0) {
-                        //Not the user-landing page
-                    } else { 
-                        if (!sessionStorage.alreadyClicked) { 
-                                $('#exampleModalCenter').modal('show');
-                                $( ".modal-title .navbar-brand" ).animate({ "left": "0" }, "slow" );
-                                sessionStorage.alreadyClicked = "true";
-                        } else{
-                                return false;
-                        }
-                    }
-                    $('.logout-button').click(function () {
-                        window.sessionStorage.clear();
-                    })
-                    // Affirmations popup code starts here - Karthik
+    if ($("#userLandingContent1").length == 0) {
+        //Not the user-landing page
+    } else {
+        if (!sessionStorage.alreadyClicked) {
+            $('#exampleModalCenter').modal('show');
+            $(".modal-title .navbar-brand").animate({
+                "left": "0"
+            }, "slow");
+            sessionStorage.alreadyClicked = "true";
+        } else {
+            return false;
+        }
+    }
+    $('.logout-button').click(function() {
+        window.sessionStorage.clear();
+    })
+    // Affirmations popup code starts here - Karthik
 
 
     //Registration breadcrumb issue fix
-    if($(".registration-dashboard").length == 0){
-        $(".step.step-1").addClass( "done" );
-    }else{
-        $(".step.step-1").removeClass( "done" );
-        $(".step.step-1").addClass( "active" );
+    if ($(".registration-dashboard").length == 0) {
+        $(".step.step-1").addClass("done");
+    } else {
+        $(".step.step-1").removeClass("done");
+        $(".step.step-1").addClass("active");
     }
 });
