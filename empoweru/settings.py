135ecd153b630bb284858be7b14f848bec11738a
Shamrose Makandar <31262996+shamrose@users.noreply.github.com>
2019-02-07 13:59:18 -0600

    Updated settings.py

--- empoweru/settings.py
+++ empoweru/settings.py
@@ -161,6 +161,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATIC_URL = 'static'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "") 
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
