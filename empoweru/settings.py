b778f681ba826f714b2b737faff65200dec4f2ed
Tarun Chaturvedi <33792060+Tarun1226@users.noreply.github.com>
2019-02-07 11:23:36 -0600

    Update settings.py

--- empoweru/settings.py
+++ empoweru/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.postgres',
-    'storages',
+    'storages'
 
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
     'widget_tweaks',
     'wagtailmenus',
     'crispy_forms',
+
 ]
 
 MIDDLEWARE = [
@@ -202,11 +203,15 @@ AWS_S3_OBJECT_PARAMETERS = {
 
 AWS_LOCATION = 'static'
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
-DEFAULT_FILE_STORAGE = 'storage_backends.MediaStorage'
 
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 AWS_HEADERS = {
     'Access-Control-Allow-Origin': '*'
