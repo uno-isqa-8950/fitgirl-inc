bbacd141ce941d50bdbe3eedbd985bed7b4a0d16
Tarun Chaturvedi <33792060+Tarun1226@users.noreply.github.com>
2019-02-07 12:06:38 -0600

    Update settings.py

--- empoweru/settings.py
+++ empoweru/settings.py
@@ -195,6 +195,8 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+MEDIA_ROOT = os.environ.get("MEDIA_URL", "")
+MEDIA_URL = '/media/'
 
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
