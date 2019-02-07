62e099f3af2f617e59c26accd058267d302b0142
Shamrose Makandar <31262996+shamrose@users.noreply.github.com>
2019-02-07 17:32:17 -0600

    Updated settings file

--- empoweru/settings.py
+++ empoweru/settings.py
@@ -212,7 +212,7 @@ MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, '/media/')
+STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 #MEDIA_URL = '/media/'
