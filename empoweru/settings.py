d9576bde5ef7cefef815679023dba887a736ac08
Shamrose Makandar <31262996+shamrose@users.noreply.github.com>
2019-02-07 02:06:15 -0600

    Update storage_backends

--- empoweru/settings.py
+++ empoweru/settings.py
@@ -177,11 +177,11 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
 
 AWS_LOCATION = 'static'
 
-STATICFILES_STORAGE = 'storages_backends.StaticStorage'
+STATICFILES_STORAGE = 'storage_backends.StaticStorage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 MEDIAFILES_LOCATION = 'media'
-DEFAULT_FILE_STORAGE = 'storages_backends.MediaStorage'
+DEFAULT_FILE_STORAGE = 'storage_backends.MediaStorage'
 
 
 AWS_HEADERS = {
