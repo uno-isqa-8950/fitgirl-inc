324e95f54a67b1c718be3a4ab0ad1ea0cf556fd6
Shamrose Makandar <31262996+shamrose@users.noreply.github.com>
2019-02-07 14:15:56 -0600

    Updating the settings

--- empoweru/settings.py
+++ empoweru/settings.py
@@ -168,8 +168,9 @@ AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "")
 AWS_QUERYSTRING_AUTH = False 
 AWS_S3_CUSTOM_DOMAIN = os.environ.get("AWS_S3_CUSTOM_DOMAIN", "")
-MEDIA_ROOT = os.environ.get("MEDIA_URL", "")
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.environ.get("MEDIA_URL", "")
+
 
 
 AWS_HEADERS = {
