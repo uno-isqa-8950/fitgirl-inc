77ade0565c677dd1c7a48b0aeaee0bd4b3a0eb57
Shamrose Makandar <31262996+shamrose@users.noreply.github.com>
2019-02-07 18:56:59 -0600

    Updating settings.py

--- empoweru/settings.py
+++ empoweru/settings.py
@@ -186,7 +186,7 @@ USE_TZ = True
 
 #S3 settings
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'media/'),
+    os.path.join(BASE_DIR, 'static/'),
 ]
 
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
