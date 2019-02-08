5786301ac42d2c0e03dca03a4db4e9cfb5116830
karthik <kbellaryvijaya@gmail.com>
2019-02-08 02:11:05 -0600

    changes made for physical redesign,fixing the art module' tile image stretching

--- account/static/js/script.js
+++ account/static/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     // Affirmations popup code starts here - Karthik
-    if ($("#userLandingContent1").length == 0) {
+    if ($(".current-week").length == 0) {
         //Not the user-landing page
     } else {
         if (!sessionStorage.alreadyClicked) {
@@ -20,11 +20,29 @@ $(document).ready(function() {
     // Affirmations popup code starts here - Karthik
 
 
-    //Registration breadcrumb issue fix
+    //Registration breadcrumb issue fix - karthik
     if ($(".registration-dashboard").length == 0) {
         $(".step.step-1").addClass("done");
     } else {
         $(".step.step-1").removeClass("done");
         $(".step.step-1").addClass("active");
     }
+
+$( "#strength .rich-text img" ).wrapAll( "<div class='strength-images-array' />");
+$( "#agility .rich-text img" ).wrapAll( "<div class='agility-images-array' />");
+$( "#flexibility .rich-text img" ).wrapAll( "<div class='flexibility-images-array' />");
+$( "#strength .rich-text p" ).wrapAll( "<div class='strength-paragraphs' />");
+$( "#agility .rich-text p" ).wrapAll( "<div class='agility-paragraphs' />");
+$( "#flexibility .rich-text p" ).wrapAll( "<div class='flexibility-paragraphs' />");
+$(".strength-paragraphs").insertAfter(".strength-images-array");
+$(".agility-paragraphs").insertAfter(".agility-images-array");
+$(".flexibility-paragraphs").insertAfter(".flexibility-images-array");
+
+
+$(".physical-post-page").closest("body").addClass("physical-body");
+$( ".physical-post-page br" ).remove();
+// $( "h2" ).appendTo( $( ".container" ) )
+
+
+
 });
