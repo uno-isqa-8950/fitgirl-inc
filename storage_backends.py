377b696c81b196d3bc42a7ab6247e567cf5b7258
Shamrose Makandar <31262996+shamrose@users.noreply.github.com>
2019-02-08 22:08:07 -0600

    Update storage_backends

--- storage_backends.py
+++ storage_backends.py
@@ -1,10 +1,6 @@
-from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
-class StaticStorage(S3Boto3Storage):
-    location = settings.AWS_LOCATION
-    
-    
 class MediaStorage(S3Boto3Storage):
-    location = settings.MEDIAFILES_LOCATION
+    location = 'media'
+    file_overwrite = False
 
