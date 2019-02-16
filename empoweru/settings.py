88d6062cfc8db4c848f7a2bc5cbbdd38cc016b1d
Shamrose Makandar <31262996+shamrose@users.noreply.github.com>
2019-02-15 21:18:43 -0600

    Update settings.py

--- empoweru/settings.py
+++ empoweru/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.postgres',
-    'letsencrypt',
+    #'letsencrypt',
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
     'wagtail.contrib.modeladmin',
