b7e34a62058db02bd3fa21df97d7fca219e84da8
Joe Lofshult <jlofshult@unomaha.edu>
2019-02-17 08:00:43 -0600

    Merged from sprint-2 branch

--- account/static/js/script.js
+++ account/static/js/script.js
@@ -18,14 +18,59 @@ $(document).ready(function() {
         window.sessionStorage.clear();
     })
     // Affirmations popup code ends here - Karthik
+// if($(".footer-from-disclaimer").length == 0){
+// }else{
+//
+//     $("#userLandingContent1").css("display","none")
+// }
 
 
-
-    //Registration breadcrumb issue fix
+    //Registration breadcrumb issue fix - karthik
     if ($(".registration-dashboard").length == 0) {
         $(".step.step-1").addClass("done");
     } else {
         $(".step.step-1").removeClass("done");
         $(".step.step-1").addClass("active");
     }
+
+$( "#strength .rich-text img" ).wrapAll( "<div class='strength-images-array' />");
+$( "#agility .rich-text img" ).wrapAll( "<div class='agility-images-array' />");
+$( "#flexibility .rich-text img" ).wrapAll( "<div class='flexibility-images-array' />");
+$("#strength .rich-text").children().not( ".strength-images-array" ).wrapAll( "<div class='strength-paragraphs' />");
+$("#agility .rich-text").children().not( ".agility-images-array" ).wrapAll( "<div class='agility-paragraphs' />");
+$("#flexibility .rich-text").children().not( ".flexibility-images-array" ).wrapAll( "<div class='flexibility-paragraphs' />");
+$(".strength-paragraphs").insertAfter(".strength-images-array");
+$(".agility-paragraphs").insertAfter(".agility-images-array");
+$(".flexibility-paragraphs").insertAfter(".flexibility-images-array");
+
+
+$(".physical-post-page").closest("body").addClass("physical-body");
+$( ".physical-post-page br" ).remove();
+
+
+
+
+
+// var phyiscaltimer = function(){
+// $('.drawer').slideDrawer({
+// showDrawer: false, // The drawer is hidden by default.
+// slideTimeout: true, // Sets the drawer to slide down after set count if set to true.
+// slideSpeed: 600, // Slide drawer speed. 
+// slideTimeoutCount: 3000, // How long to wait before sliding drawer slides down.
+// });
+// };
+// Code for toggling the timer
+// var r = $("<input/>").attr({ type: "a", id: "field", class:" start-timer btn btn-sm btn-pink btn-rounded waves-effect waves-light",value:"start timer",href:"#"});
+// $(".strength-paragraphs").append(r);
+// $(".physical-post-page").on('click', '.start-timer', function(){
+// //  $(".demo").slideToggle();
+// $(".demo").css("visibility","visible");
+// //$('#done').show();
+
+// });
+// function activityDone(){
+//     alert("done");
+// }
 });
+
+  
