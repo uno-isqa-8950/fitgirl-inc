a4ec5f2bda06e701e22ac29f724f149203804d22
khd20 <kduong@unomaha.edu>
2019-02-22 20:38:35 -0600

    enhancement on individual emails

--- account/forms.py
+++ account/forms.py
@@ -85,7 +85,7 @@ class EmailForm(forms.Form):
 
 class ContactForm(forms.Form):
     subject = forms.CharField(required=True)
-    contact_email = forms.EmailField(required=True)
+    #contact_email = forms.EmailField(required=True)
     message = forms.CharField(required=True, widget=forms.Textarea)
 
 
