4a21aad25c00d42bc7ea0c01e525f0e297a004cd
Hemachandra <35674174+hcghanta@users.noreply.github.com>
2019-03-03 23:15:37 -0600

    path change for pdf

--- account/views.py
+++ account/views.py
@@ -519,7 +519,7 @@ def rewards_redeem(request):
                                                                         'points_available': points_available,
                                                                         'reward_number': reward_number})
             out = BytesIO()
-            stylesheets = [weasyprint.CSS(settings.STATIC_CSS + '/css/pdf.css')]
+            stylesheets = [weasyprint.CSS(settings.STATIC_URL + '/css/pdf.css')]
             print(stylesheets)
             weasyprint.HTML(string=html).write_pdf(out,stylesheets=stylesheets)
             email.attach('Redemption No. {}'.format(rewards.reward_no), out.getvalue(), 'application/pdf')
