0b68d26e39dcf3123f18e040f677939a259c4944
Hemachandra <35674174+hcghanta@users.noreply.github.com>
2019-03-03 22:54:04 -0600

    removed redundant code

--- account/models.py
+++ account/models.py
@@ -94,11 +94,6 @@ class Profile(models.Model):
             return int((datetime.now().date() - self.date_of_birth).days / 365.25)
 
 
-    def get_context(self, value):
-        context = super(Profile, self).get_context(value)
-        user1=User.objects.get(username=request.user.username)
-        context['points'] = (value['points'] == user1.Profile)
-
 class Parameters(models.Model):
     physical_days_to_done = models.IntegerField(default=1)
     nutrition_days_to_done = models.IntegerField(default=1)
