2c5dc98568f6d2554dd2d85667ed7b4be863e136
Shamrose Makandar <31262996+shamrose@users.noreply.github.com>
2019-04-09 21:39:31 -0500

    Update models.py

--- account/models.py
+++ account/models.py
@@ -232,10 +232,11 @@ class CloneProgramInfo(models.Model):
     active = models.BooleanField(default=False, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
+ '''   
 class School(models.Model):
     school_id = models.AutoField(primary_key=True,blank=False,null=False)
     school_name = models.CharField(max_length=25, blank=True, null=True)
 
     def __str__(self):
         return str(self.school_name)
-
+'''
