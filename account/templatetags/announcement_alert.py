5661e46f98f451f4e39666a076cc0f4c7f53963c
Shamrose Makandar <31262996+shamrose@users.noreply.github.com>
2019-04-17 19:26:06 -0500

    Update announcement_alert.py

--- account/templatetags/announcement_alert.py
+++ account/templatetags/announcement_alert.py
@@ -8,9 +8,4 @@ register = template.Library()
 
 @register.inclusion_tag('account/announcement_alert.html')
 def announcement_alert():
-    today = datetime.date.today()
-    print(today)
-    alert_message = AnnouncementAlertPage.objects.filter(start_date__lte=today) & AnnouncementAlertPage.objects.filter(end_date__gte=today)
-    for alerts in alert_message:
-        alert_data = alerts.announcements
-        return {'alert_data':alert_data}
+    pass
