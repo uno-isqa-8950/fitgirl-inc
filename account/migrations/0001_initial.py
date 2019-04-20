438f1a5264a60f6dc209d7b8c01d06a5911d0f75
shamrose <smakandar@unomaha.edu>
2019-04-20 12:02:33 -0500

    Add the files to Sprint-6 backup

--- account/migrations/0001_initial.py
+++ account/migrations/0001_initial.py
@@ -24,18 +24,7 @@ class Migration(migrations.Migration):
                 ('published_date', models.DateField(null=True)),
             ],
         ),
-        migrations.CreateModel(
-            name='CloneProgramInfo',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('program_to_clone', models.CharField(max_length=25)),
-                ('new_start_date', models.DateField()),
-                ('new_program', models.CharField(max_length=25)),
-                ('active', models.BooleanField(default=False)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('user', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL)),
-            ],
-        ),
+
         migrations.CreateModel(
             name='Dailyquote',
             fields=[
