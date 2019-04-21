8ccbaa3a76cf7dab8e1c759c7b6a6b8f76ddb19d
hcghanta <hghanta@unomaha.edu>
2019-04-21 13:18:34 -0500

    html directories

--- account/templatetags/unread_messages.py
+++ account/templatetags/unread_messages.py
@@ -5,9 +5,9 @@ from django.contrib.auth.models import User
 register = template.Library()
 
 
-@register.inclusion_tag('account/user_message.html',takes_context=True)
+@register.inclusion_tag('account/../templates/email/user_message.html', takes_context=True)
 def unread_message(context):
     request = context['request']
     user = User.objects.get(email=request.user.email)
     unread_count = KindnessMessage.objects.filter(to_user=user).filter(read_message=False).count()
-    return {'unread_count':unread_count}
+    return {'unread_count': unread_count}
