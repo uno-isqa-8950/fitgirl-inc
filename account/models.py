2ba1e5e95eabce9c4e0f9320bd71aa2ee5dbd371
Hitesh Kumar Khatri <46361248+hkhatri005@users.noreply.github.com>
2020-04-04 22:13:14 -0500

    Removed Default Password

--- account/models.py
+++ account/models.py
@@ -236,20 +236,3 @@ class Schools(models.Model):
     def __str__(self):
         return str(self.schools_name)
 
-
-# # Model to save default password
-# class DefaultPassword(models.Model):
-#     id = models.IntegerField(primary_key=True, blank=False, null=False)
-#     default_password = models.CharField(max_length=30, blank=False)
-
-#     def __str__(self):
-#         return str(self.default_password)
-
-
-# # Model to save default welcome email
-# class WelcomeEmail(models.Model):
-#     welcome_email = models.CharField(max_length=1000, blank=False)
-
-#     def __str__(self):
-#         return str(self.welcome_email)
-
