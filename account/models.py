b710901c6fe8992cbf24b8cf0c82c063dbe6d8d0
Hitesh Kumar Khatri <46361248+hkhatri005@users.noreply.github.com>
2020-04-13 16:25:19 -0500

    Update models.py

--- account/models.py
+++ account/models.py
@@ -30,7 +30,7 @@ BACKGROUND_CHOICES = [
 class KindnessCardTemplate(models.Model):
     id = models.AutoField(null=False, primary_key=True, blank = False)
     image_name = models.CharField(max_length=25, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True, upload_to="static/images/")
+    image = models.ImageField(null=True, blank=True, upload_to="images")
 
     def __str__(self):
         return str(self.image_name)
